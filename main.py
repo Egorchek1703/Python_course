5c5cab124ef4588cf406a05488cbfbbb5b7817c0
Egor Semenov <Semenow.egor.andreevich@mail.ru>
2024-12-02 03:37:43 +0300

    Урок 2: Переменные и работа с ними

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-print("Hello World form first line")
-print("Hello World form second line")
+# Variables
+
+a = "Hello"
+b = 25
+
+"""
+    Это многострочный комментарий
+"""
+# А это однострочный
+
+print(a)
+print(b)
+
+c = 42
+c = 33
+print(c) # Выведет 33
+
+num1 = 1
+num2 = 41
+result = num1 + num2
+print(result) # Выведет 42
+
+first_name = "Egor"
+last_name = "Semenov"
+full_name = first_name + " " + last_name
+print(full_name)
+
+
+string = "I am a string "
+number = 42
+print(string + str(number))
