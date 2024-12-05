c9c2e0a835bb2fa4c8d9b55aead649e91d7e824e
Egor Semenov <Semenow.egor.andreevich@mail.ru>
2024-12-06 02:18:56 +0300

    Урок 6: Массивы и функционал работы с ними

--- main.py
+++ main.py
@@ -1,12 +1,63 @@
-# Cycles
+# Arrays (lists)
+#=============================== Part 1 ====================================
 
-for i in range(0, 100):
-    print(i)
+cities = ['Voronej', 'Moscow', 'ryazan', 'Smolensk', 'Taganrog']
 
-for i in range (0, 100 + 1, 20):
-    print(i) # 0 20 40 60 80 100
+name_of_list = list(range(0, 30))
 
-x = 0
-while x <= 100:
-    print(x)
-    x = x + 0.5
+print(name_of_list)
+
+print( len(name_of_list) )
+
+print( cities[2].title() ) # Ryazan
+print( cities[2].upper() ) # RYAZAN
+print( cities[3].lower() ) # smolensk
+
+cities[2] = 'Ryazan_2'
+print(cities) # выведет массив содержащий третим элементом строку 'Ryazan_2'
+
+
+cities.append('Sevastopol\'')
+print(cities)
+
+cities.insert(2, 'Ryazan_1,5')
+print(cities)
+
+del cities[2] # удалит 'Ryazan_1,5'
+print(cities)
+
+cities.remove('Ryazan_2') # удалит 'Ryazan_2'
+print(cities)
+
+cities.sort()
+name_of_list.sort(reverse=True)
+
+print(cities)
+print(name_of_list)
+
+#=============================== Part 2 ====================================
+
+cars = ['lada', 'VW', 'bmv', 'mersedes', 'kia', 'skoda']
+for car in cars:
+    print(car.upper())
+
+
+my_number_list = list(range(0, 10))
+for num in my_number_list:
+    num = num * 2
+    print(num)
+
+
+cars = ['lada', 'VW', 'bmv', 'mersedes', 'kia', 'skoda']
+
+german_cars = cars[1:4]
+print("German cars: " + str(german_cars))
+
+three_cars = cars[:3]
+print("First 3 cars: " + str(three_cars))
+
+some_cars = cars[2:]
+print("Some cars: " + str(some_cars))
+
+copy_of_list = cars[:]
+print(copy_of_list)
