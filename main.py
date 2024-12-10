59983c8a06d8a0613ee5cacb03a87ae319cc6636
Egor Semenov <Semenow.egor.andreevich@mail.ru>
2024-12-10 03:44:38 +0300

    Урок 10: Функции

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-# Input
+# Functions
 
-name = input("Write your name: ")
-print("Привет " + name + "!")
+def say_hello():
+    print("Hello")
 
-a = input("a = ") # введем 4
-b = input("b = ") # введем 2
-print(a + b) # 42
-print( int(a) + int(b) ) # 6
+say_hello()
 
 
-password = ''
-while True:
-    password = input('Password: ')
-    if(password != '123'):
-        print('Password "' + password + '" is not correct')
+
+a = int(input('a = '))
+b = int(input('b = '))
+def sum():
+    return a + b
+
+result = sum()
+print(result)
+
+
+
+def multiply_by_2(num):
+    return(num * 2)
+
+print( multiply_by_2(5) ) # 10
+
+
+
+def calc_factorial(n):
+    """Calculate factorial of numbers"""
+    if (n == 1):
+        return 1
     else:
-        print('You are authorized!')
-        break
+        return n * calc_factorial(n - 1)
 
-print('Cycle has been finished')
+factorial_num = int( input("What number do you want to calculate factorial of? ") )
+print( calc_factorial(factorial_num) )
+
+
+print('==================================')
+def safe_user_data(*arguments):
+    user = []
+    for piece_of_data in arguments:
+        user.append(piece_of_data)
+    return user
+print( safe_user_data('Egor', '24', 'DevOps', 'Hachapuri') )
+print('==================================')
+
+print('==================================')
+def safe_user_data(**arguments):
+    user = arguments
+    return user
+print( safe_user_data(name='Egor', age='24', work='DevOps', dishes='Hachapuri') )
+print('==================================')
+
+def create_list_and_dict(static_arg, static_arg2, *unnamed_args, **named_args):
+    print(static_arg)
+    print(static_arg2)
+    print(unnamed_args)
+    print(named_args)
+
+create_list_and_dict(1, 2, 'I', 'am', 'a', 'big', 'lover', 'of', 'python', '!', name='Egor', age=24, phone='8(800)555-35-35')
